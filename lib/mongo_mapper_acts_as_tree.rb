06af4af11618b19f70c5636ffd6e7a508cd9d7ed
Joel Junström <joel.junstrom@oktavilla.se>
2011-05-23 12:43:46 +0200

    Updated class inheritable methods to use class_attribute

--- lib/mongo_mapper_acts_as_tree.rb
+++ lib/mongo_mapper_acts_as_tree.rb
@@ -4,11 +4,11 @@ module MongoMapper
   module Acts
     module Tree
       def self.included(model)
-        model.class_eval do 
+        model.class_eval do
           extend InitializerMethods
         end
       end
-      
+
       module InitializerMethods
         def acts_as_tree(options = {})
           options = {
@@ -16,42 +16,42 @@ module MongoMapper
             :path_field      => "path",
             :depth_field     => "depth"
           }.merge(options)
-          
-          write_inheritable_attribute :acts_as_tree_options, options
-          class_inheritable_reader :acts_as_tree_options
-          
+
+          class_attribute :acts_as_tree_options, :instance_writer => false
+          self.acts_as_tree_options = options
+
           include InstanceMethods
           include Fields
           extend Fields
           extend ClassMethods
-          
+
           key parent_id_field,  ObjectId
           key path_field,       Array,  :default => [], :index => true
           key depth_field,      Integer, :default => 0
-          
+
           after_save      :move_children
           validate        :will_save_tree
           before_destroy  :destroy_descendants
         end
       end
-      
+
       module ClassMethods
         def roots
           self.all(parent_id_field => nil, :order => tree_order)
         end
       end
-      
+
       module InstanceMethods
         def ==(other)
           return true if other.equal?(self)
           return true if other.instance_of?(self.class) and other._id == self._id
           false
         end
-      
+
         def parent=(var)
-          self[parent_id_field] = nil if var.nil?   
+          self[parent_id_field] = nil if var.nil?
           var = search_class.find(var) if var.is_a? String
-          
+
           if self.descendants.include? var
             @_cyclic = true
           else
@@ -60,13 +60,13 @@ module MongoMapper
             @_will_move = true
           end
         end
-        
+
         def will_save_tree
           if @_cyclic
             errors.add(:base, "Can't be children of a descendant")
           end
         end
-        
+
         def fix_position
           if parent.nil?
             self[parent_id_field] = nil
@@ -78,73 +78,73 @@ module MongoMapper
             self[depth_field] = parent[depth_field] + 1
           end
         end
-        
+
         def parent
           @_parent or (self[parent_id_field].nil? ? nil : search_class.find(self[parent_id_field]))
         end
-        
+
         def root?
           self[parent_id_field].nil?
         end
-        
+
         def root
           self[path_field].first.nil? ? self : search_class.find(self[path_field].first)
         end
-        
+
         def ancestors
           return [] if root?
           search_class.find(self[path_field])
         end
-        
+
         def self_and_ancestors
           ancestors << self
         end
-        
+
         def siblings
           search_class.all(:_id => {"$ne" => self._id}, parent_id_field => self[parent_id_field], :order => tree_order)
         end
-        
+
         def self_and_siblings
           search_class.all(parent_id_field => self[parent_id_field], :order => tree_order)
         end
-        
+
         def children
           search_class.all(parent_id_field => self._id, :order => tree_order)
         end
-        
+
         def descendants
           return [] if new_record?
           search_class.all(path_field => self._id, :order => tree_order)
         end
-        
+
         def self_and_descendants
           [self] + self.descendants
         end
-        
+
         def is_ancestor_of?(other)
           other[path_field].include?(self._id)
         end
-        
+
         def is_or_is_ancestor_of?(other)
           (other == self) or is_ancestor_of?(other)
         end
-        
+
         def is_descendant_of?(other)
           self[path_field].include?(other._id)
         end
-        
+
         def is_or_is_descendant_of?(other)
           (other == self) or is_descendant_of?(other)
         end
-        
+
         def is_sibling_of?(other)
           (other != self) and (other[parent_id_field] == self[parent_id_field])
         end
-        
+
         def is_or_is_sibling_of?(other)
           (other == self) or is_sibling_of?(other)
         end
-        
+
         def move_children
           if @_will_move
             @_will_move = false
@@ -155,29 +155,29 @@ module MongoMapper
             @_will_move = true
           end
         end
-        
+
         def destroy_descendants
           search_class.destroy(self.descendants.map(&:_id))
         end
       end
-      
+
       module Fields
         def parent_id_field
           acts_as_tree_options[:parent_id_field]
         end
-        
+
         def path_field
           acts_as_tree_options[:path_field]
         end
-        
+
         def depth_field
           acts_as_tree_options[:depth_field]
         end
-        
+
         def tree_order
           acts_as_tree_options[:order] or ""
         end
-        
+
         # When added as an option to acts_as_tree, search class will be used as the base from which to
         # find tree objects. This is handy should you have a tree of objects that are of different types, but
         # might be related through single table inheritance.
